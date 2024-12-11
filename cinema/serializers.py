a407dbb342cde2c77b80fbe1c5948ae2c735f792
mregorlite <egor.infinity@gmail.com>
2024-12-11 15:50:23 +0200

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,103 @@
-# write serializers here
+from rest_framework import serializers
+from rest_framework.fields import ReadOnlyField
+
+from cinema.models import Genre, CinemaHall, Actor, Movie, MovieSession
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = (
+            "id",
+            "name",
+            "rows",
+            "seats_in_row",
+            "capacity",
+        )
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("id", "name")
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name", "full_name")
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = (
+            "id",
+            "title",
+            "description",
+            "duration",
+            "genres",
+            "actors",
+        )
+
+
+class MovieListSerializer(serializers.ModelSerializer):
+    genres = serializers.StringRelatedField(many=True)
+    actors = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Movie
+        fields = (
+            "id",
+            "title",
+            "description",
+            "duration",
+            "genres",
+            "actors",
+        )
+
+
+class MovieDetailSerializer(serializers.ModelSerializer):
+    genres = GenreSerializer(many=True, read_only=True)
+    actors = ActorSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Movie
+        fields = (
+            "id",
+            "title",
+            "description",
+            "duration",
+            "genres",
+            "actors",
+        )
+
+
+class MovieSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovieSession
+        fields = (
+            "show_time",
+            "movie",
+            "cinema_hall"
+        )
+
+
+class MovieSessionListSerializer(serializers.ModelSerializer):
+    movie = MovieListSerializer()
+    cinema_hall = CinemaHallSerializer()
+    movie_title = ReadOnlyField(source="movie.title")
+    cinema_hall_name = ReadOnlyField(source="cinema_hall.name")
+    cinema_hall_capacity = ReadOnlyField(source="cinema_hall.capacity")
+
+    class Meta:
+        model = MovieSession
+        fields = (
+            "id",
+            "show_time",
+            "movie_title",
+            "cinema_hall_name",
+            "cinema_hall_capacity",
+            "movie",
+            "cinema_hall",
+        )
